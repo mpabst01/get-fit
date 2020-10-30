6d64eda3c81833957aaea8fb07d757d6ddc3acbd
Marylee Pabst <marylee@uconn.edu>
2020-10-30 09:28:01 -0700

    add html routes

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,58 @@
+const Workout = require("../Develop/models/WorkoutModel");
+const Mongoose  = require("mongoose");
+
+const Range = function findRange(workouts) {
+    workoutS = []
+    const emptyWorkout = {
+        exercises : [],
+        timeSpentMakingGains: 0
+    }
+    var dayOfTheWeek = new Date(Date.now()).getDay()
+    for (x = 0; x <= dayOfTheWeek; x++) {
+        if (workouts[x]) {
+            workoutS.unshift(workouts[x]);
+        }
+        else {
+            workoutS.unshift(emptyWorkout);
+        };
+    }
+
+    return workoutS;
+}
+
+module.exports = function (app) {
+    console.log('Engaging api-routes.js');
+
+    app.get("/api/workouts", async (req, res) => {
+        var workouts = await workOut.find();
+        for (const workout of workouts) {
+            await workout.setTotalDuration()
+        }
+        res.json(workouts);
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        Workout.create(req.body).then(data => {
+            res.json(data)
+        })
+    });
+
+    app.put("/api/workouts/:id", async (req, res) => {
+        var workoutId = req.params.id
+        Workout.findOneAndUpdate({ _id: Mongoose.Types.ObjectId(workoutId) }, {
+            $push: {
+                exercises: req.body
+            }
+        }).then(data => {
+            res.json(data)
+        })
+        
+    });
+
+    app.get("/api/workouts/range", async (req, res) => {
+        console.log('workouts and range api route.');
+        Workout.find().then(data => {
+            res.json(data);
+        }).catch(err => alert(err))
+    });
+}
---
+++ Develop/routes/html-routes.js
@@ -0,0 +1,19 @@
+  
+const path = require('path')
+
+module.exports = function(app) {
+    console.log('utilizing html-routes.js');
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "./public/exercise.html"));
+    });
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "./public/index.html"));
+    });
+
+    app.get("/stats", (req, res) => {
+        console.log('running stats html route');
+        res.sendFile(path.join(__dirname, "./public/stats.html"));
+    });
+
+}
