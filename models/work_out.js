a5059c845fc3b4614e6f6f666e37782eff4d3c97
Marylee Pabst <marylee@uconn.edu>
2020-10-29 19:52:46 -0700

    schema stuff

--- models/work_out.js
+++ models/work_out.js
@@ -1,2 +1,34 @@
 const mongoose = require("mongoose");
+const Exercise = require("./exercise");
+
 const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    type: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    duration: {
+        type: Number,
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+});
+
+const workOut = new Schema("Work out", {
+
+    day: {},
+    exercise: {},
+    duration: {}
+
+
+});
