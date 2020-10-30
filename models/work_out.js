760b8aa7a1ce22005f46bf99c6ffc43fd225a771
Marylee Pabst <marylee@uconn.edu>
2020-10-29 20:00:24 -0700

    call model

--- models/work_out.js
+++ models/work_out.js
@@ -10,25 +10,45 @@ const ExerciseSchema = new Schema({
     name: {
         type: String,
     },
+    sets: {
+        type: Number,
+
+    },
+    reps: {
+        type: Number,
+    },
+
     duration: {
         type: Number,
     },
     weight: {
         type: Number,
     },
-    reps: {
-        type: Number,
-    },
-    sets: {
-        type: Number,
-    },
-});
-
-const workOut = new Schema("Work out", {
-
-    day: {},
-    exercise: {},
-    duration: {}
-
 
 });
+
+const workOutSchema = new Schema("Work out", {
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: {
+        type: Array,
+        default: []
+    },
+    timeSpentMakingGains: {
+        type: Number,
+        default: 0
+    },
+    weightTotal: {
+        type: Number,
+        default: 0
+    }
+
+
+});
+
+
+const wod = mongoose.model("Workout", workOutSchema);
+module.exports = wod;
